1b6b937c8d8802fb1386e1343b9ccdc96df08249
elmotabuzo16 <elmotabuzojr.ust@gmail.com>
2019-11-26 09:55:11 +0800

    Exercise: Two Way Binding - Disable button if input is empty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http'
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ServerComponent
+      ServerComponent,
+      UserComponent
    ],
    imports: [
       BrowserModule,
